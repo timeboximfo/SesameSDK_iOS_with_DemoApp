8d3096d811c51dfd6d1d2f07141a79f5f959e97a
timeboximfo <timebox.imfo@gmail.com>
2025-09-13 14:31:59 +0900

    Package.swift

--- Package.swift
+++ Package.swift
@@ -3,18 +3,15 @@ import PackageDescription
 
 let package = Package(
     name: "SesameSDK",
-    platforms: [
-        .iOS(.v12)
-    ],
+    platforms: [.iOS(.v12)],
     products: [
         .library(name: "SesameSDK", targets: ["SesameSDK"]),
         .library(name: "AESc", targets: ["AESc"])
     ],
-    // ★ AWS SDK を安定版で依存追加（2.41.0 以降）
     dependencies: [
         .package(
             url: "https://github.com/aws-amplify/aws-sdk-ios-spm",
-            .upToNextMajor(from: "2.41.0")
+            .upToNextMajor(from: "2.41.0")   // 2.41.0 以上
         )
     ],
     targets: [
@@ -28,15 +25,12 @@ let package = Package(
             dependencies: [
                 "AESc",
                 .product(name: "AWSCore", package: "aws-sdk-ios-spm"),
-                // ★ これが不足していた
                 .product(name: "AWSCognitoIdentityProvider", package: "aws-sdk-ios-spm"),
+                .product(name: "AWSAPIGateway", package: "aws-sdk-ios-spm"),   // ← これを追加
                 .product(name: "AWSIoT", package: "aws-sdk-ios-spm")
-                // ※ AWSAPIGateway は SesameSDK のコードで使っていなければ不要です
             ],
             path: "Sources/SesameSDK",
-            resources: [
-                .process("DB/CHDeviceModel.xcdatamodeld")
-            ]
+            resources: [.process("DB/CHDeviceModel.xcdatamodeld")]
         )
     ]
 )
