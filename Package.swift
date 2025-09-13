114ad9a98ffb0605aeb90f6d66f20f9edc7e8837
timeboximfo <timebox.imfo@gmail.com>
2025-09-13 13:30:04 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,36 +1,32 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
-    name: "SesameSDK",
-    platforms: [
-        .iOS(.v12)
-    ],
-    products: [
-        .library(name: "SesameSDK", targets: ["SesameSDK"]),
-        .library(name: "AESc", targets: ["AESc"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/aws-amplify/aws-sdk-ios-spm", .upToNextMajor(from: "2.0.0"))
-    ],
-    targets: [
-        .target(
-            name: "AESc",
-            path: "Sources/AESc",
-            publicHeadersPath: "."
-        ),
-        .target(
-            name: "SesameSDK",
-            dependencies: [
-                "AESc",
-                .product(name: "AWSCore", package: "aws-sdk-ios-spm"),
-                .product(name: "AWSAPIGateway", package: "aws-sdk-ios-spm"),
-                .product(name: "AWSIoT", package: "aws-sdk-ios-spm")
-            ],
-            path: "Sources/SesameSDK",
-            resources: [
-                .process("DB/CHDeviceModel.xcdatamodeld")
-            ]
-        )
-    ]
+  name: "SesameSDK",
+  platforms: [
+    .iOS(.v12), .macOS(.v10_15), .watchOS(.v7)
+  ],
+  products: [
+    .library(name: "SesameSDK", targets: ["SesameSDK"]),
+  ],
+  // ★ AWS SDK を依存に追加
+  dependencies: [
+    .package(url: "https://github.com/aws-amplify/aws-sdk-ios-spm",
+             .upToNextMajor(from: "2.41.0"))
+  ],
+  targets: [
+    .target(
+      name: "SesameSDK",
+      dependencies: [
+        "AESc",
+        .product(name: "AWSCore", package: "aws-sdk-ios-spm"),
+        .product(name: "AWSCognitoIdentityProvider", package: "aws-sdk-ios-spm"),
+        .product(name: "AWSIoT", package: "aws-sdk-ios-spm")
+      ],
+      resources: [
+        .process("DB/CHDeviceModel.xcdatamodeld")
+      ]
+    ),
+    .target(name: "AESc")
+  ]
 )
